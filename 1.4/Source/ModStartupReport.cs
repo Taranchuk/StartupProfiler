687fcf66ffd2b1d8f09eda11b56e0cec0ca9bf35
Phil <supersmashphil@gmail.com>
2023-01-26 22:20:07 -0700

    rebuild and cleanup from merge

--- 1.4/Source/ModStartupReport.cs
+++ 1.4/Source/ModStartupReport.cs
@@ -62,62 +62,7 @@ namespace ModStartupImpactStats
             if (ModStartupImpactStatsMod.stopwatch != null)
             {
                 ModStartupImpactStatsMod.stopwatch.Stop();
-                //mainMessage.AppendLine("Mods installed: " + ModLister.AllInstalledMods.Where(x => x.Active).Count() + " - total startup time: " + ModStartupImpactStatsMod.stopwatch.Elapsed.ToString(@"m\:ss"));
                 Summary.TotalElapsed = ModStartupImpactStatsMod.stopwatch.Elapsed;
-                if (Prefs.LogVerbose)
-                {
-                    //Don't need to deconstruct, ModImpactData has reference to declaring ModContentPack
-                    foreach (ModImpactData modImpactData in Summary.AllEntries.OrderByDescending(modImpactData => modImpactData.TotalImpactTime))
-					{
-                        if (modImpactData.TotalImpactTime >= ModImpactData.MinModImpactLogging && !modImpactData.mod.IsOfficialMod)
-						{
-						}
-					}
-                    /*
-                    var allCategoryImpacts = new Dictionary<string, float>();
-                    foreach (var modImpact in ModImpactData.modsImpact.OrderByDescending(x => x.Value.TotalImpactTime))
-                    {
-                        var impactTime = modImpact.Value.TotalImpactTime;
-                        if (impactTime > ModImpactData.MinModImpactLogging)
-                        {
-                            var packageId = modImpact.Key;
-                            var mod = LoadedModManager.RunningMods.FirstOrDefault(x => x.PackageIdPlayerFacing.ToLower() == packageId.ToLower());
-                            if (mod != null)
-                            {
-                                if (!mod.IsOfficialMod)
-                                {
-                                    secondaryMessage.AppendLine("Mod impact: " + mod.Name + " - " + impactTime.ToStringDecimalIfSmall() + "s, summary:\n" + modImpact.Value.ModSummary());
-                                }
-                            }
-                            else
-                            {
-                                secondaryMessage.AppendLine("Total impact: - " + packageId + " - " + impactTime.ToStringDecimalIfSmall() + "s, summary:\n" + modImpact.Value.ModSummary());
-                            }
-                        }
-
-                    foreach (var category in modImpact.Value.impactByCategories)
-                    {
-                        if (allCategoryImpacts.ContainsKey(category.Key))
-                        {
-                            allCategoryImpacts[category.Key] += category.Value.Sum(x => x.Value);
-                        }
-                        else
-                        {
-                            allCategoryImpacts[category.Key] = category.Value.Sum(x => x.Value);
-                        }
-                    }
-                }
-
-                    foreach (var category in allCategoryImpacts.OrderByDescending(x => x.Value))
-                    {
-                        mainMessage.AppendLine("Category " + category.Key + " took " + (category.Value).ToStringDecimalIfSmall() + "s");
-                    }
-                    mainMessage.AppendLine("Mod impact measured: " + (ModImpactData.modsImpact.Sum(x => x.Value.TotalImpactTime()).ToStringDecimalIfSmall() + "s"));
-                    */
-                    //DisableXMlOnlyMods(mess);
-                }
-                var modReport = "Mod info report: \n" + mainMessage.ToString() + "\n" + secondaryMessage.ToString();
-                //Log.Warning(modReport);
             }
         }
 
